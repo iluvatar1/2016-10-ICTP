e8e183d72bd7f25bab54c3e52dc8469a74100227
woquendo <woquendo@hp83-inf-13.ictp.it>
2016-10-07 15:00:24 +0200

    Added check

--- 10-07/matrix-mpi.c
+++ 10-07/matrix-mpi.c
@@ -5,18 +5,21 @@
 #define SIZE 4
 
 void initialize(double * M, int size);
+void print(double * M, int size);
+void check(int size);
 
 int main(int argc, char **argv)
 {
-  double * A = malloc(SIZE*SIZE*sizeof(double));
+  double * A = malloc(SIZE*SIZE*sizeof(double)); // does not scale for large matrices
   
   // initialize matrix
   initialize(A, SIZE);
 
   // checking
-  
-  // print matrix
+  check(SIZE);
 
+  // print matrix
+  print(A, SIZE);
 
   return 0;
 }
@@ -30,4 +33,35 @@ void initialize(double * M, int size)
       else M[ii*size + jj] = 0.0;
     }
   }
+}
+
+void print(double * M, int size)
+{
+  FILE *fp;
+  fp = fopen( "matrix.out", "w+b" );
+  fwrite( M, sizeof(double), size*size, fp); 
+  fclose( fp );
+}
+
+void check(int size)
+{
+  // does not scale for large matrices
+  double * M = malloc(size*size*sizeof(double));
+  
+  // read matrix
+  FILE *fp;
+  fp = fopen( "matrix.out", "r+b" );
+  fread( M, sizeof(double), size*size, fp); 
+  fclose( fp );
+  
+
+  // compare with unit matrix
+  int ii, jj;
+  double sum = 0.0;
+  for (ii = 0; ii < size; ++ii){
+    for (jj = 0; jj < size; ++jj){
+      sum += abs(M[ii*size + jj] - ((ii == jj) ? 1.0 : 0.0) ); 
+    }
+  }
+  printf("------------- diff = %.12le -------------\n", sum);
 }
